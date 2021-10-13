1034700e114e02525571b617cfcf608e6a95013d
Ramsey Njire <ramseynjire@gmail.com>
2021-10-13 21:11:26 +0300

    write #try_guess method

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -43,4 +43,23 @@ class Hangman
       end
     end
   end
+
+  def try_guess(char)
+    if already_attempted?(char)
+      print "That has already been attempted"
+      return false
+    else
+      @attempted_chars << char
+    end
+
+    matching_indices = get_matching_indices(char)
+
+    if matching_indices.length == 0
+      @remaining_incorrect_guesses -= 1
+    else
+      fill_indices(char, matching_indices)
+    end
+
+    true
+  end
 end
